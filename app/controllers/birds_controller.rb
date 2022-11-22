fcec3792149b486e4863270ca436ed79b31e2906
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-11-22 16:58:27 +0300

    custom routes in rails

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -21,6 +21,35 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+ 
+  def update 
+    # find the bird
+    new_bird = Bird.find_by(id: params[:id])
+
+    # found /birds
+    if new_bird
+      new_bird.update(bird_params)
+      render json: new_bird, status: :accepted
+      
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+
+  end
+
+  def increment_likes
+    # find
+    bird_like = Bird.find_by(id: params[:id])
+
+    if bird_like
+      bird_like.update(likes: bird_like.likes + 1)
+      render json: bird_like
+
+      # not found
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
 
   private
 
